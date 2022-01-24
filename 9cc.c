afa484dd9424a4259f12011211f914819245ce9a
yamanoi-shu <surayama1111@gmail.com>
2022-01-25 00:43:20 +0900

    ステップ3 トークナイズによって空白文字除去

--- 9cc.c
+++ 9cc.c
@@ -1,5 +1,110 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <string.h>
+
+
+typedef enum {
+  TK_RESERVED, // 記号
+  TK_NUM, // 数字
+  TK_EOF, // 入力の終わり
+} TokenKind;
+
+
+typedef struct Token Token;
+
+struct Token {
+  TokenKind kind; // トークンの型
+  Token *next; // 次のトークン
+  int val; // kindがTK_NUMの場合、その数値
+  char *str; // トークン文字列
+};
+
+Token *token;
+
+void error(char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  vfprintf(stderr, fmt, ap);
+  exit(1);
+}
+
+
+// tokenが記号の場合にtokenを次のトークンへ進めて、trueを返す
+// それ以外はfalseを返す
+bool consume(char op) {
+  if (token->kind != TK_RESERVED || token->str[0] != op) {
+    return false;
+  }
+  token = token->next;
+  return true;
+}
+
+// tokenが記号の場合にtokenを次のトークンへ進めて、trueを返す
+// それ以外はerror()を呼ぶ
+void expect(char op) {
+  if (token->kind != TK_RESERVED || token->str[0] != op) {
+    error("'%c'ではありません", op);
+  }
+  token = token->next;
+}
+
+// tokenが数値の場合にtokenを次のトークンへ進めて、その数値を返す
+// それ以外はerror()を呼ぶ
+int expect_number() {
+  if (token->kind != TK_NUM) {
+    error("数ではありません。");
+  }
+  int val = token->val;
+  token = token->next;
+  token->val = val;
+  return val;
+}
+
+//tokenが入力の終わりかを返す
+bool at_eof() {
+  return token->kind == TK_EOF;
+}
+
+// 新しいトークンを生成してcurの次に連結する
+Token *new_token(TokenKind kind, Token *cur, char *str) {
+  Token *tok = calloc(1, sizeof(Token));
+  tok->kind = kind;
+  tok->str = str;
+  cur->next = tok;
+  return tok;
+}
+
+// 入力文字列pをトークナイズしてそれを返す
+Token *tokenize(char *p) {
+  Token head;
+  head.next = NULL;
+  Token *cur = &head;
+
+  while(*p) {
+    if (isspace(*p)) {
+      p++;
+      continue;
+    }
+    if (*p == '+' || *p == '-') {
+      cur = new_token(TK_RESERVED, cur, p++);
+      continue;
+    }
+
+    if (isdigit(*p)) {
+      cur = new_token(TK_NUM, cur, p);
+      cur->val = strtol(p, &p, 10);
+      continue;
+    }
+
+    error("トークナイズできません");
+  }
+
+  new_token(TK_EOF, cur, p);
+  return head.next;
+}
 
 int main(int argc, char **argv) {
   if (argc != 2) {
@@ -7,29 +112,23 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  char *p = argv[1];
+  // トークナイズする
+  token = tokenize(argv[1]);
 
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
 
-  printf(" mov rax, %ld\n", strtol(p, &p, 10));
-  while (*p) {
-    if (*p == '+') {
-      p++;
-      printf(" add rax, %ld\n", strtol(p, &p, 10));
-      continue;
-    }
-    if (*p == '-') {
-      p++;
-      printf(" sub rax, %ld\n", strtol(p, &p, 10));
-      continue;
-    }
+  printf("  mov rax, %d\n", expect_number());
 
-    fprintf(stderr, "予期しない文字です: '%c'\n", *p);
-    return 1;
+  while (!at_eof()) {
+    if (consume('+')) {
+      printf("  add rax, %d\n", expect_number());
+    }
+    expect('-');
+    printf("  sub rax, %d\n", expect_number());
   }
 
-  printf(" ret\n");
+  printf("  ret\n");
   return 0;
 }
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # 9cc 勉強用レポジトリ
 参考 https://www.sigbus.info/compilerbook
+
+If you want to run on Mac, run `make`.
